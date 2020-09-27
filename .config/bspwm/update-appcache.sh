728426d5d13156bd7b21af0291664d0993a08048
Hakan Erduman <smultimeter@gmail.com>
2020-09-27 15:21:46 +0200

    add border for vte

--- .config/bspwm/update-appcache.sh
+++ .config/bspwm/update-appcache.sh
@@ -1,22 +1,19 @@
 #!/bin/sh
-
-#IFS=':' 
-#grep -rcv ^NoDisplay=true /usr/share/applications ~/.local/share/applications | while read f c; do
-    #name=$(grep ^Name= $f | head -n1 | cut -b6-)
-    #if [ -n "$name" ]; then
-        #echo -n "${name}\t${f}\t" >> ~/.cache/applications
-        #grep ^Exec= $f | cut -b6- >> ~/.cache/applications || echo $f is broken: $name
-    #fi
-#done
+# update the application cache for use with dmenu
 
 usr=/usr/share/applications
 loc=~/.local/share/applications
 self=~/.config/bspwm/update-appcache.sh
 cache=~/.cache/applications
 
-if [ $# -eq 0 ];then    
-    rm -rf ~/.cache/applications
-    find $usr $loc -type f -name '*.desktop' -exec $0 {} \;
+if [ $# -eq 0 ];then
+    if [ $cache -nt $usr -a $cache -nt $loc ]; then
+        echo app cache is up to date.
+        exit 0
+    fi
+    rm -rf ${cache}.new # just in case
+    find $usr $loc -type f -name '*.desktop' -exec $0 {} \; # call self with arg
+    mv -vf ${cache}.new ${cache} # atomic
 else
     f=$*
     if [ -z "$f" -o ! -f "$f" ] || grep -q '^OnlyShowIn=\|^DontShowIn=' "$f"; then 
@@ -25,7 +22,7 @@ else
     fi
     name=$(grep ^Name= "$f" | head -n1 | cut -b6-)
     if [ -n "$name" ]; then
-        echo -n "${name}\t${f}\t" >> $cache
+        echo -n "${name}\t${f}\t" >> ${cache}.new
         grep ^Exec= "$f" | head -n1 | cut -b6- >> $cache || echo broken: $name: $f
         echo add: $name $f
     fi
