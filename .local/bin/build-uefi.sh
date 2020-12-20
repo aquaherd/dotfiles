0da29ef1d9d7168c1ce8342b22b5d11f42c51cab
Hakan Erduman <smultimeter@gmail.com>
2020-12-20 13:58:30 +0100

    Minor fixes in sway

--- .local/bin/build-uefi.sh
+++ .local/bin/build-uefi.sh
@@ -20,12 +20,31 @@ INITRD="$MTP/initrd.img"
 # Set the EFI STUB path, the default value should be good for the most users 
 STUB="$HOME/.local/lib/linuxx64.efi.stub"
 
+TARGET=/boot/efi/EFI/Linux/$ID.efi
+
+if [ ! -f $VMLINUZ -o ! -f $INITRD ]; then
+	echo "Setup invalid"
+	exit 1
+fi
+
+if [ -e $TARGET ]; then
+	if [ $TARGET -nt $VMLINUZ -a $TARGET -nt $INITRD ]; then
+		echo "$TARGET is already up-to-date."
+		exit 0
+	fi
+else
+	echo "Will update $TARGET"
+fi
+
+if [ -n "$MTP"  ]; then
+	cd $MTP
+fi
 objcopy \
     --add-section .osrel="$OS_RELEASE" --change-section-vma .osrel=0x20000 \
     --add-section .cmdline="/tmp/kernel-command-line.txt" --change-section-vma .cmdline=0x30000 \
     --add-section .splash="$SPLASH" --change-section-vma .splash=0x40000 \
     --add-section .linux="$VMLINUZ" --change-section-vma .linux=0x2000000 \
     --add-section .initrd="$INITRD" --change-section-vma .initrd=0x3000000 \
-    "$STUB" "$ID.efi"
+    "$STUB" "/tmp/$ID.efi" || echo FAIL && exit 1
 
-sudo cp -vu $ID.efi /boot/efi/EFI/Linux
+sudo cp -vu /tmp/$ID.efi $TARGET && rm /tmp/$ID.efi
