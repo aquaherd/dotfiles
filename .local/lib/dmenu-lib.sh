3f5c8a033dd28b09d0b0649fa149f0437a0218f3
Hakan Erduman <hakan@erduman.de>
2021-05-13 18:41:41 +0200

    Recheck sway on alpine without display manager

--- .local/lib/dmenu-lib.sh
+++ .local/lib/dmenu-lib.sh
@@ -1,6 +1,22 @@
 # this is a posix shell fragment for inclusion not execution
-ask()
+
+fix_desktop() # in case you start without display manager
 {
+	if [ -z "$DESKTOP_SESSION" ]; then
+		if [ -n "$SWAYSOCK" ]; then
+			export DESKTOP_SESSION=sway
+			return
+		elif [ -n "$I3SOCK" ]; then
+			export DESKTOP_SESSION=i3
+			return
+		fi
+	fi
+}
+
+ask() # bifurcate to dmenu or dmenu-wl
+{
+	fix_desktop
+    
     case $DESKTOP_SESSION in
     sway)
         m=0
@@ -11,11 +27,11 @@ ask()
             fi
             m=$((1+m))
         done
-        dmenu-wl -i -p $1 -fn 'Iosevka Term 15' -nb '#44475a' -sb '#bd93f9' -h 30 -m $m -b
+        dmenu-wl -i -p $1 -fn 'Iosevka 15' -nb '#44475a' -sb '#bd93f9' -h 30 -m $m -b
         ;;
     hikari)
         # dont know how to determine current monitor in hikari
-        dmenu-wl -i -p $1 -fn 'Iosevka Term 15' -nb '#44475a' -sb '#bd93f9' -h 30 -b
+        dmenu-wl -i -p $1 -fn 'Iosevka 15' -nb '#44475a' -sb '#bd93f9' -h 30 -b
         ;;
     *)
         ${DMENU_DEFAULTS:='-fn sans:fontformat=truetype:pixelsize=20 -nb #44475a -sb #bd93f9'}
