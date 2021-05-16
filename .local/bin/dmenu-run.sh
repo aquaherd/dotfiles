4c6940509ce0575eab08f89e872a6e17bf51dc6b
Hakan Erduman <smultimeter@gmail.com>
2021-05-16 12:31:11 +0200

    Better quoting for dmenu-run and indexing

--- .local/bin/dmenu-run.sh
+++ .local/bin/dmenu-run.sh
@@ -2,9 +2,9 @@
 . ~/.local/lib/dmenu-lib.sh
 #dmenu_path | dmenu -i $DMENU_DEFAULTS | ${SHELL:-"/bin/sh"} &
 res=$(cut -f1 ~/.cache/bspwm/applications | sort -u  | ask 'drun:') || exit 0
-desktop=$(basename $(grep "$res" ~/.cache/bspwm/applications | cut -f2))
-echo "dmenu-run.sh: $desktop"
-gtk-launch $desktop
+desktop=$(basename "$(grep "$res" ~/.cache/bspwm/applications | cut -f2)")
+echo "dmenu-run.sh: $res -> $desktop"
+gtk-launch "$desktop"
 fail=$?
 if [ $fail -ne 0 ]; then
     zenity --error --text "error $fail: $err"
