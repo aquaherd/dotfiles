55c0d9f8a138a39deaf9aebe16b5686b89622998
Hakan Erduman <smultimeter@gmail.com>
2021-06-04 16:07:53 +0200

    who needs compton

--- .local/bin/dmenu-run.sh
+++ .local/bin/dmenu-run.sh
@@ -1,7 +1,7 @@
 #/bin/sh
 . ~/.local/lib/dmenu-lib.sh
 #dmenu_path | dmenu -i $DMENU_DEFAULTS | ${SHELL:-"/bin/sh"} &
-res=$(cut -f1 ~/.cache/bspwm/applications | sort -u  | ask 'drun:') || exit 0
+res=$(cut -f1 ~/.cache/applications.txt | sort -u  | ask 'drun:') || exit 0
 desktop=$(basename "$(grep "$res" ~/.cache/bspwm/applications | cut -f2)")
 echo "dmenu-run.sh: $res -> $desktop"
 gtk-launch "$desktop"
