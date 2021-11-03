230c9bc5f4dae77a8ee09fc0b62c40ae7c510c3a
Hakan Erduman <smultimeter@gmail.com>
2021-11-03 18:30:06 +0100

    some cleanups and fixes

--- .local/lib/chroot.sh
+++ .local/lib/chroot.sh
@@ -3,15 +3,25 @@ chroot_mount() {
 	sudo mkdir -p /run/mount/$2
     sudo mount -t auto $1 /run/mount/$2
     local d
-	for d in boot/efi dev dev/pts etc/resolv.conf proc run sys tmp; do
+	for d in boot/efi dev dev/pts proc run sys tmp etc/resolv.conf; do
 		if [ -e /run/mount/$2/$d ]; then
-			sudo mount -o bind /$d /run/mount/$2/$d
+			sudo mount -o bind /$d /run/mount/$2/$d || echo mount failure in $d
+			continue
 		fi
+		if [ -h /run/mount/$2/$d ]; then
+		    local l
+		    l=$(readlink /run/mount/$2/$d)
+		    sudo mkdir -vp /run/mount/$2/$(dirname $l) || echo mkdir failure $l
+		    sudo touch /run/mount/$2$l || echo touch failure $l
+		    sudo mount -o bind /$d /run/mount/$2/$l || echo mount failure in $d -> $l
+		    continue
+		fi
+	    echo not mounting $d
 	done
 }
 
 chroot_umount() {
 	echo "chroot_umount $1 from $2"
-    sudo fstrim -v /run/mount/$2
+    #sudo fstrim -v /run/mount/$2
     sudo umount --recursive --lazy /run/mount/$2 && sudo rmdir /run/mount/$2
 }
