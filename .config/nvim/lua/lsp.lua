eebad4fcbb0f8fa890923a64e9993e2e4ae44cd5
Hakan Erduman <hakan@erduman.de>
2022-10-07 18:39:00 +0200

    i3 resize and nvim mappings

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -5,12 +5,19 @@ vim.diagnostic.config({
 })
 -- Mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
-local opts = { noremap = true, silent = true }
--- vim.api.nvim_set_keymap('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
-vim.api.nvim_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
-vim.api.nvim_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
-vim.api.nvim_set_keymap('n', '<leader>q', '<cmd>lua vim.diagnostic.setqflist()<CR>', opts)
-vim.api.nvim_set_keymap('n', '<leader>h', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
+
+local function gmap(mode, keys, command, desc)
+    vim.api.nvim_set_keymap(mode, keys, command, {noremap = true, silent = true, desc = desc})
+end
+
+local function bmap(bufnr, mode, keys, command, desc)
+    vim.api.nvim_buf_set_keymap(bufnr, mode, keys, command, {noremap = true, silent=true,desc=desc})
+end
+
+gmap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', "lsp: go to previous diagnostic")
+gmap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', "lsp: go to next diagnostic")
+gmap('n', '<leader>q', '<cmd>lua vim.diagnostic.setqflist()<CR>', "lsp: show diagnostics in qflist")
+gmap('n', '<leader>h', '<cmd>lua vim.diagnostic.open_float()<CR>', "lsp: show diagnostic in float")
 
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
@@ -20,21 +27,17 @@ local on_attach = function(_, bufnr)
 
   -- Mappings.
   -- See `:help vim.lsp.*` for documentation on any of the below functions
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-  vim.keymap.set('n', '<leader>wl', function()
-    vim.inspect(vim.lsp.buf.list_workspace_folders())
-  end, opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>f', '<cmd>lua vim.lsp.buf.format()<CR>', opts)
+  bmap(bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', "lsp: signature help")
+  bmap(bufnr, 'n', '<leader>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', "lsp: go to type definition")
+  bmap(bufnr, 'n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', "lsp: code action")
+  bmap(bufnr, 'n', '<leader>f', '<cmd>lua vim.lsp.buf.format()<CR>', "lsp: format buffer")
+  bmap(bufnr, 'n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', "lsp: rename symbol")
+  bmap(bufnr, 'n', 'F2', '<cmd>lua vim.lsp.buf.rename()<CR>', "lsp: rename symbol")
+  bmap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', "lsp: hover help")
+  bmap(bufnr, 'n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', "lsp: go to declaration")
+  bmap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', "lsp: go to definition")
+  bmap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', "lsp: go to implementation")
+  bmap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', "lsp: go to references")
 end
 
 -- Add additional capabilities supported by nvim-cmp
