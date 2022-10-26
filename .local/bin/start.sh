d6a217d5d965d886c0001ede244e0441a21ca820
Hakan Erduman <hakan@erduman.de>
2022-10-26 20:40:43 +0200

    start/stop.sh

--- .local/bin/start.sh
+++ .local/bin/start.sh
@@ -1,31 +1,27 @@
 #!/bin/sh
 hostpath="$XDG_RUNTIME_DIR/host"
-host=win106
-usbf=$host
-
-if [ $# -eq 1 ]; then
-    host=$1
-    usbf=zsc
+if [ $# -eq 0 ]; then
+    echo "usage: start.sh HOST"
+    exit 1
 fi
-
+host=$1
+ssh $host echo OK
 for d in Firmware Logfiles; do
-        if mountpoint -q ~/$d; then
-                echo unmounting \~/$d ...
-                fusermount -u ~/$d
-        fi
-        echo mounting ${host}:$d to \~/$d ...
-        sshfs ${host}:$d ~/$d
+    if mountpoint -q ~/$d; then
+        echo "unmounting \~/$d ..."
+        fusermount -u ~/$d
+    fi
+    echo "mounting ${host}:$d to \~/$d ..."
+    sshfs ${host}:$d ~/$d
 done
 echo $host > "$hostpath"
-ssh -O forward $host '2323 192.168.194.40:23'
-ssh -O forward $usbf "3240 $host:3240"
 for b in $(sudo usbip list -r localhost 2> /dev/null |grep UART|cut -d':' -f1); do
-  case $b in
-    *-*)
-      echo "attaching UART $b"
-      sudo usbip attach -r localhost -b $b
-      ;;
-    *) continue;;
-  esac
+    case $b in
+        *-*)
+            echo "attaching UART $b"
+            sudo usbip attach -r localhost -b $b
+            ;;
+        *) continue;;
+    esac
 done
 echo OK
