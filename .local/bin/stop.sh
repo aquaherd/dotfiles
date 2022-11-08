9d16e6980ac98f94d0f90c16cfecca32c41fef0a
Hakan Erduman <hakan@erduman.de>
2022-11-08 17:10:42 +0100

    refactor work sh

--- .local/bin/stop.sh
+++ .local/bin/stop.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 hostpath="$XDG_RUNTIME_DIR/host"
 for d in Firmware Logfiles; do
-        echo unmount $d
+    echo unmount $d
     fusermount -u ~/$d || echo 'was not mounted'
 done
 if test -f "$hostpath"; then
-  read host < "$hostpath" || echo no host set
-  echo "exit session $host"
-  ssh -qO exit "$host"
-  rm "$hostpath"
+    read host < "$hostpath" || echo no host set
+    echo "exit session $host"
+    ssh -qO exit "$host"
+    rm "$hostpath"
 fi
 echo OK
