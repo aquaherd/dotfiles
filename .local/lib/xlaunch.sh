aa3ec44901c9301588ba5caaec9ce37c11f90e47
Hakan Erduman <hakan@erduman.de>
2023-03-25 14:28:57 +0100

    getting rid of greeters

--- .local/lib/xlaunch.sh
+++ .local/lib/xlaunch.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 # run minimum needful things before starting the WM
-# see ~/.local/bin.startX
+# see ~/.local/bin/startX
 
 test -f ~/.Xresources && xrdb -merge ~/.Xresources
 xsetroot -cursor_name left_ptr
-
+if [ -z "$XDG_RUNTIME_DIR" ]; then
+    export XDG_RUNTIME_DIR=/run/user/$(id -u)
+    sudo mkdir -p $XDG_RUNTIME_DIR
+    sudo chown $USER:$USER $XDG_RUNTIME_DIR
+    chmod 0700 $XDG_RUNTIME_DIR
+fi
+if [ -z "$DBUS_SESSION_BUS_ADDRESS" ]; then
+    exec dbus-run-session $1
+fi
 exec $1
