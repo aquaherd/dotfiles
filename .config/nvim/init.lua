ee491f460208771bc36a4607df54601d18e6755e
Hakan Erduman <hakan.erduman@siemens-healthineers.com>
2023-06-12 16:37:37 +0200

    nvim: fidget use legacy

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -136,7 +136,7 @@ require("lazy").setup({
     {
         'neovim/nvim-lspconfig',
         dependencies = { 'williamboman/mason.nvim', 'williamboman/mason-lspconfig.nvim',
-            { 'j-hui/fidget.nvim', opts = {} }, 'folke/neodev.nvim', },
+            { 'j-hui/fidget.nvim', opts = {}, tag = 'legacy' }, 'folke/neodev.nvim', },
     },
     { 'hrsh7th/nvim-cmp',       dependencies = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip' }, },
     -- non-lua
