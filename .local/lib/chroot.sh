73d42bbb72be737be4351b8250fcab13e2fd6714
Hakan Erduman <hakan@erduman.de>
2023-08-15 13:00:06 +0200

    chroot: fix symlinked resolv.conf

--- .local/lib/chroot.sh
+++ .local/lib/chroot.sh
@@ -9,8 +9,17 @@ chroot_mount()
   done
   echo "chroot_mount $1 to $2"
   $sudo mkdir -p /run/mount/"$2"
-  $sudo mount -t auto "$1" /run/mount/"$2"
+  $sudo mount -t auto "$1" /run/mount/"$2" || return 1
+  if ! $sudo chroot /run/mount/"$2" true; then
+    echo "$2 was not chrootable"
+    return 1
+  fi
   local d
+  if [ -L /run/mount/"$2"/etc/resolv.conf ]; then
+    echo "Moving symlinked resolv.conf away"
+    $sudo mv /run/mount/"$2"/etc/resolv.conf /run/"$2"-resolv.conf
+    $sudo touch /run/mount/"$2"/etc/resolv.conf
+  fi
   for d in mnt/data boot/efi dev dev/pts proc run sys tmp etc/resolv.conf tmp/.X11-unix; do
     if [ -e /run/mount/"$2"/$d ]; then
       $sudo mount -o bind /$d /run/mount/"$2"/$d || echo mount failure in $d
@@ -37,5 +46,9 @@ chroot_umount()
 {
   echo "chroot_umount $1 from $2"
   #$sudo fstrim -v /run/mount/$2
+  if [ -L /run/"$2"-resolv.conf ]; then
+    echo "Restoring symlinked resolv.conf"
+    $sudo mv /run/"$2"-resolv.conf /run/mount/"$2"/etc/
+  fi
   $sudo umount --recursive --lazy "/run/mount/$2" && $sudo rmdir "/run/mount/$2"
 }
