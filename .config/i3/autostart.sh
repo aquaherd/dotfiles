d4eade5c2a93b61e9f161fdd22d330552781cb8d
Hakan Erduman <hakan.erduman@siemens-healthineers.com>
2023-09-18 11:12:52 +0200

    i3: barrier

--- .config/i3/autostart.sh
+++ .config/i3/autostart.sh
@@ -5,6 +5,8 @@ randr_restore
 restore_backdrop
 pids="$XDG_RUNTIME_DIR/i3"
 mkdir "$pids"
+exec > $pids/autostart.log
+exec 2>&1
 start()
 {
     echo "restart: $*"
@@ -14,7 +16,7 @@ start()
     echo $pid > "$pids/$1.pid"
 
 }
-for d in "$XDG_RUNTIME_DIR"/i3/*.pid; do 
+for d in $pids/*.pid; do 
     read -r pid < "$d"
     kill "$pid"
     rm "$pid"
@@ -28,7 +30,9 @@ start xsettingsd -c ~/.config/i3/xsettings.conf
 start i3-autotiling 
 start xsetroot -cursor_name left_ptr
 start stalonetray
-BARRIERRC="~/.config/barrier/$HOSTNAME.conf"
+BARRIERRC=~/.config/barrier/${HOSTNAME}.conf
 if test -f "$BARRIERRC"; then
     start barriers -f --debug INFO --name "$HOSTNAME" --enable-crypto -c "$BARRIERRC" --address '[127.0.0.1]:24800'
+else
+    echo "$BARRIERRC does not exist"
 fi
