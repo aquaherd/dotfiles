26a3fdb90f8467f416bfc08bd76f1e06dea527c6
Hakan Erduman <hakan@erduman.de>
2023-11-23 18:46:03 +0100

    some font and ui changes

--- .local/bin/update-appcache.sh
+++ .local/bin/update-appcache.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # update the application cache for use with dmenu
 
 usr=/usr/share/applications
@@ -7,23 +7,23 @@ fpk=~/.local/share/flatpak/exports/share/applications
 cache=~/.cache/applications.txt
 
 if [ $# -eq 0 ];then
-    if [ $cache -nt $usr -a $cache -nt $loc -a $cache -nt $fpk ]; then
+    if [ $cache -nt $usr ] && [ $cache -nt $loc ] && [ $cache -nt $fpk ]; then
         echo app cache is up to date.
         exit 0
     fi
     rm -rf ${cache}.new # just in case
-    find $usr $loc $fpk -name '*.desktop' -exec $0 {} \; # call self with arg
+    find $usr $loc $fpk -name '*.desktop' -exec "$0" {} \; # call self with arg
     mv -vf ${cache}.new ${cache} # atomic
 else
     f=$*
-    if [ -z "$f" -o ! -f "$f" ] || grep -q '^OnlyShowIn=\|^DontShowIn=\|^NoDisplay=true' "$f"; then 
-        echo skip: $f
+    if [ -z "$f" ] || [ ! -f "$f" ] || grep -q '^OnlyShowIn=\|^DontShowIn=\|^NoDisplay=true' "$f"; then 
+        echo skip: "$f"
         exit 0
     fi
     name=$(grep ^Name= "$f" | head -n1 | cut -b6-)
     if [ -n "$name" ]; then
         printf '%s\t%s\n' "${name}" "${f}" >> ${cache}.new
-        echo add: $name $f
+        echo "add: $name $f"
     fi
 fi
 
