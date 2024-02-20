93648cfae93724bcd794988a493b82158a575d7c
Hakan Erduman <hakan.erduman@siemens-healthineers.com>
2024-02-20 19:49:17 +0100

    wsld: finalize

--- .bashrc.d/wsl2.sh
+++ .bashrc.d/wsl2.sh
@@ -1,31 +1,29 @@
 #!/bin/bash
 if command -v wsld > /dev/null; then
+	export DISPLAY=:0
 	if ! pgrep wsld > /dev/null; then
-		export DISPLAY=:0
 		nohup wsld > /dev/null < /dev/null 2>&1 &
 		disown
 		# sleep until $DISPLAY is up
 		while ! xset q > /dev/null 2>&1 ; do
 			sleep 0.3
 		done	
-		xrdb -merge ~/.Xresources
-		# create XDG
-		if [[ -z "$XDG_RUNTIME_DIR" ]]; then
-			export XDG_RUNTIME_DIR=/run/user/$UID
+	fi
+	xrdb -merge ~/.Xresources
+	# create XDG
+	if [[ -z "$XDG_RUNTIME_DIR" ]]; then
+		export XDG_RUNTIME_DIR=/run/user/$UID
+		if [[ ! -d "$XDG_RUNTIME_DIR" ]]; then
+			export XDG_RUNTIME_DIR=/run/shm/$UID
 			if [[ ! -d "$XDG_RUNTIME_DIR" ]]; then
-				export XDG_RUNTIME_DIR=/run/shm/$UID
-				if [[ ! -d "$XDG_RUNTIME_DIR" ]]; then
-					mkdir -m 0700 "$XDG_RUNTIME_DIR"
-				fi
-			fi
-		else
-			export PROFILE_LOADED=1
-		fi
-		if [ -z "$DBUS_SESSION_ADDRESS" ]; then
-			export DBUS_SESSION_ADDRESS=unix:path=$XDG_RUNTIME_DIR/bus
-			if [ ! -S $XDG_RUNTIME_DIR/bus ]; then
-				dbus-daemon --session --fork --address=$DBUS_SESSION_ADDRESS
+				mkdir -m 0700 "$XDG_RUNTIME_DIR"
 			fi
 		fi
+	else
+		export PROFILE_LOADED=1
+	fi
+	export DBUS_SESSION_ADDRESS=unix:path=$XDG_RUNTIME_DIR/bus
+	if ! pgrep dbus > /dev/null; then
+		dbus-daemon --session --fork --address=$DBUS_SESSION_ADDRESS
 	fi
 fi
