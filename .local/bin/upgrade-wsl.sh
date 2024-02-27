944ad8a323cd3c2f21196438527f30734a990b36
Hakan Erduman <z003tnse@SHS4830642C>
2024-02-27 08:54:58 +0100

    wsl: drop symlink

--- .local/bin/upgrade-wsl.sh
+++ .local/bin/upgrade-wsl.sh
@@ -1,1 +1,23 @@
-/home/z003tnse/OneDrive/wsl/upgrade.sh
+#!/bin/sh
+if ! command -v wsl.exe > /dev/null; then
+	echo 'This script runs under wsl only - wsl.exe not found.'
+	exit 1
+fi
+for d in $(wsl.exe -l -q|tr -d "\r\000" |sort -f); do
+
+	case $d in
+	jessie|stretch|docker*|podman*)
+		echo ... Skipping "$d" ...
+		echo
+		continue
+		;;
+	*)
+
+		CMD="wsl.exe -d $d --cd /etc grep ^ID= os-release"
+		eval "$($CMD)"
+		echo ... Updating "$d" ID="$ID" ...
+		wsl.exe -d "$d" --cd /root -u root sh /home/"$USER"/.local/lib/upgrade-"${ID}".sh
+		echo
+		;;
+	esac
+done
