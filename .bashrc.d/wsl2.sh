18f6ed7bfe340b6eaefc46d9c9bb1a9839ad8bc8
Hakan Erduman <hakan.erduman@siemens-healthineers.com>
2024-02-28 15:49:37 +0100

    wsl: deduplicate wsl daemons and unblock startup

--- .bashrc.d/wsl2.sh
+++ .bashrc.d/wsl2.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
+# XDG
+if [[ -z "$XDG_RUNTIME_DIR" ]]; then
+	export XDG_RUNTIME_DIR=/run/user/$UID
+	if [[ ! -d "$XDG_RUNTIME_DIR" ]]; then
+		export XDG_RUNTIME_DIR=/run/shm/$UID
+		if [[ ! -d "$XDG_RUNTIME_DIR" ]]; then
+			mkdir -m 0700 "$XDG_RUNTIME_DIR"
+		fi
+	fi
+else
+	export PROFILE_LOADED=1
+fi
+# X11
 if command -v wsld > /dev/null; then
 	export DISPLAY=:0
 	if ! pgrep wsld > /dev/null; then
+		echo 'launching wsld...'
+		rm -rf /tmp/.X*
 		nohup wsld > /dev/null < /dev/null 2>&1 &
 		disown
 		# sleep until $DISPLAY is up
@@ -10,24 +25,17 @@ if command -v wsld > /dev/null; then
 		done	
 	fi
 	xrdb -merge ~/.Xresources
-	# create XDG
-	if [[ -z "$XDG_RUNTIME_DIR" ]]; then
-		export XDG_RUNTIME_DIR=/run/user/$UID
-		if [[ ! -d "$XDG_RUNTIME_DIR" ]]; then
-			export XDG_RUNTIME_DIR=/run/shm/$UID
-			if [[ ! -d "$XDG_RUNTIME_DIR" ]]; then
-				mkdir -m 0700 "$XDG_RUNTIME_DIR"
-			fi
-		fi
-	else
-		export PROFILE_LOADED=1
-	fi
 	export DBUS_SESSION_ADDRESS=unix:path=$XDG_RUNTIME_DIR/bus
 	if ! pgrep dbus > /dev/null; then
+		echo 'launching dbus...'
 		dbus-daemon --session --fork --address=$DBUS_SESSION_ADDRESS
 	fi
 fi
-
+# SSH
 if command -v wsl2-ssh-agent > /dev/null; then
-	eval "$(wsl2-ssh-agent)"
+	export SSH_AUTH_SOCK=$XDG_RUNTIME_DIR/ssh-agent.sock 
+	if ! pgrep wsl2-ssh-agent > /dev/null; then
+		echo 'launching ssh-agent...'
+		eval "$(wsl2-ssh-agent --socket $SSH_AUTH_SOCK)"
+	fi
 fi
