cc855438eaa52080deb6e94e5233aa0ae6802691
Hakan Erduman <hakan@erduman.de>
2024-05-18 18:53:21 +0200

    i3: back to bar

--- .config/i3/autostart.sh
+++ .config/i3/autostart.sh
@@ -1,11 +1,12 @@
 #!/bin/bash 
 PATH+=:~/.local/bin
+# shellcheck source=/dev/null
 . ~/.local/lib/dmenu-lib.sh
 randr_restore
 restore_backdrop
 pids="$XDG_RUNTIME_DIR/i3"
 mkdir "$pids"
-exec > $pids/autostart.log
+exec > "$pids/autostart.log"
 exec 2>&1
 start()
 {
@@ -16,7 +17,7 @@ start()
     echo $pid > "$pids/$1.pid"
 
 }
-for d in $pids/*.pid; do 
+for d in "$pids"/*.pid; do 
     read -r pid < "$d"
     kill "$pid"
     rm "$pid"
@@ -29,7 +30,6 @@ start dunst
 start xsettingsd -c ~/.config/i3/xsettings.conf
 start i3-autotiling 
 start xsetroot -cursor_name left_ptr
-start stalonetray
 BARRIERRC=~/.config/barrier/${HOSTNAME}.conf
 if test -f "$BARRIERRC"; then
     start barriers -f --debug INFO --name "$HOSTNAME" --enable-crypto -c "$BARRIERRC" --address '[127.0.0.1]:24800'
