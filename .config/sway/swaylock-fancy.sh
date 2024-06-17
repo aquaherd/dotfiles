d39a19b7f36fd662425702054483e5fbd0eb468b
Hakan Erduman <hakan@erduman.de>
2024-06-17 19:54:20 +0200

    swaylock-fancy: single monitor bliss

--- .config/sway/swaylock-fancy.sh
+++ .config/sway/swaylock-fancy.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 # fancify later
-exec swaylock -f -c 000000 -lki ~/.cache/update-hsetroot-backdrop
+FONT="$(convert -list font | awk "{ a[NR] = \$2 } /family: $(fc-match sans -f "%{family}\n")/ { print a[NR-1]; exit }")"
+EFFECT=(-filter Gaussian -resize 20% -define filter:sigma=1.5 -resize 500.5%)
+IMAGE=~/.cache/swaylock-fancy
+grim -o DP-1 "$IMAGE"
+convert "$IMAGE" "${EFFECT[@]}" -font "$FONT" -pointsize 250 -fill white -stroke lightgray -annotate +256+512 "Login:" "$IMAGE"
+echo $PRIMARY $SECONDARY $MODE
+exec swaylock -f -c 000000 -lki "$IMAGE"
