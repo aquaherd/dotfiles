99900b55d385db548f8c01de5cbcfc06e1dcf335
Hakan Erduman <hakan.erduman@siemens-healthineers.com>
2024-06-24 13:29:10 +0200

    sway: pc198

--- .config/sway/swaylock-fancy.sh
+++ .config/sway/swaylock-fancy.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # fancify later
+
+echo $PRIMARY $SECONDARY $MODE
 FONT="$(convert -list font | awk "{ a[NR] = \$2 } /family: $(fc-match sans -f "%{family}\n")/ { print a[NR-1]; exit }")"
 EFFECT=(-filter Gaussian -resize 20% -define filter:sigma=1.5 -resize 500.5%)
 IMAGE=~/.cache/swaylock-fancy
-grim -o DP-1 "$IMAGE"
+grim -o "${1:-DP-1}" "$IMAGE"
 convert "$IMAGE" "${EFFECT[@]}" -font "$FONT" -pointsize 250 -fill white -stroke lightgray -annotate +256+512 "Login:" "$IMAGE"
-echo $PRIMARY $SECONDARY $MODE
 exec swaylock -f -c 000000 -lki "$IMAGE"
