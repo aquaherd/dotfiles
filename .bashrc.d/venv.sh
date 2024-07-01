9c05c977dc13f3c833f64f6ec23bca97c7658dfe
Hakan Erduman <hakan.erduman@siemens-healthineers.com>
2024-07-01 18:28:04 +0200

    venv: no noise if not exist

--- .bashrc.d/venv.sh
+++ .bashrc.d/venv.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-a=$(ls -1 ~/.venv)
-for dir in $a; do
-	#shellcheck disable=SC2139
-	alias "$dir"=". ~/.venv/$dir/bin/activate"
-done
+if [ -d ~/.venv ]; then
+	a=$(ls -1 ~/.venv)
+	for dir in $a; do
+		#shellcheck disable=SC2139
+		alias "$dir"=". ~/.venv/$dir/bin/activate"
+	done
+fi
