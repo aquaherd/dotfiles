6372678ebd4a5cbc79bfff695f112e79d6780bc4
Hakan Erduman <hakan@erduman.de>
2024-07-06 17:37:42 +0200

    boot: fix splash

--- .local/bin/build-uefi.sh
+++ .local/bin/build-uefi.sh
@@ -13,14 +13,16 @@ if [ ! -d /sys/firmware/efi ]; then
     exit 0
 fi
 
-echo "root=LABEL=$ID rw rootfstype=ext4 quiet splash" > /tmp/kernel-command-line.txt
+CMDLINE="root=LABEL=$ID rw rootfstype=ext4 quiet"
 VMLINUZ="$MTP/vmlinuz"
 INITRD="$MTP/initrd.img"
 STUB="$HOME/.local/lib/linuxx64.efi.stub"
 BMP="$HOME/.local/lib/$ID.bmp"
 if [ -f "$BMP" ]; then
 	STUB="--add-section .splash=$BMP --change-section-vma .splash=0x40000 $STUB"
+	CMDLINE="$CMDLINE splash vt.handoff=7 gfxpayload=keep"
 fi
+echo "$CMDLINE" > /tmp/kernel-command-line.txt
 TARGET=/boot/efi/EFI/Linux/$ID.efi
 DISK=/dev/$(lsblk -no pkname /dev/disk/by-label/EF00)
 PART=$(stat -c '%T' "$(realpath /dev/disk/by-label/EF00)")
@@ -44,12 +46,13 @@ fi
 
 echo "Building image for $PRETTY_NAME disk $DISK part $PART"
 sudo chmod +r "$VMLINUZ" "$INITRD"
+# shellcheck disable=SC2086
 objcopy \
     --add-section .osrel="$OS_RELEASE" --change-section-vma .osrel=0x20000 \
     --add-section .cmdline="/tmp/kernel-command-line.txt" --change-section-vma .cmdline=0x30000 \
     --add-section .linux="$VMLINUZ" --change-section-vma .linux=0x2000000 \
     --add-section .initrd="$INITRD" --change-section-vma .initrd=0x3000000 \
-    "$STUB" "/tmp/$ID.efi" || ( echo FAIL && exit 1 )
+    $STUB "/tmp/$ID.efi" || ( echo FAIL && exit 1 )
 
 sudo cp -vu /tmp/$ID.efi $TARGET && rm /tmp/$ID.efi
 
