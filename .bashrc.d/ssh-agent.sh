d3054dc48e21920b72fc5878672ffa36c4eed499
Hakan Erduman <hakan@erduman.de>
2024-11-27 13:09:23 +0100

    ssh-agent: alpine proof; sway: transparency

--- .bashrc.d/ssh-agent.sh
+++ .bashrc.d/ssh-agent.sh
@@ -1,6 +1,15 @@
 if [ "$SSH_AUTH_SOCK" = "" ]; then
 	export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.sock"
 fi
-if [ ! -S "$SSH_AUTH_SOCK" ] || ! ssh-add -l > /dev/null 2>&1; then
-	eval $(ssh-agent -a "$SSH_AUTH_SOCK")
+if [ ! -S "$SSH_AUTH_SOCK" ]; then
+	eval $(ssh-agent -a "$SSH_AUTH_SOCK" &>/dev/null )
+	trap "ssh-agent -k" exit
 fi
+
+ssh-add -l &>/dev/null
+
+case $? in
+	2)eval $(ssh-agent -a "$SSH_AUTH_SOCK" &>/dev/null);;
+	1)ssh-add &>/dev/null;;
+	0);;
+esac
