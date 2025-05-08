c32d7adaf0d9cbaae09ae9a34e819a870260d959
Hakan Erduman <hakan.erduman@siemens-healthineers.com>
2025-05-08 20:00:04 +0200

    shared-aliases: cdgr (cd git root)

--- .bashrc.d/shared-aliases.sh
+++ .bashrc.d/shared-aliases.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-alias quit='ssh -O exit '
+alias cdgr='cd $(git rev-parse --show-toplevel||echo .)'
 alias cfg='git --git-dir=$HOME/.cfg --work-tree=$HOME '
 alias curr='cat "$XDG_RUNTIME_DIR/host" 2> /dev/null || echo none'
+alias quit='ssh -O exit '
 alias rnvim='nvim --remote-ui --server 127.0.0.1:8192 '
 
 colors()
