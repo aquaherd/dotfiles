ff5d407814ff471b3811e9790390074e5140fa1d
Hakan Erduman <hakan.erduman@siemens-healthineers.com>
2025-06-26 17:28:30 +0200

    mini.nvim for mvim alias

---
+++ .config/mvim/init.lua
@@ -0,0 +1,204 @@
+-- Put this at the top of 'init.lua'
+local path_package = vim.fn.stdpath('data') .. '/site'
+local mini_path = path_package .. '/pack/deps/start/mini.nvim'
+---@diagnostic disable-next-line: undefined-field
+if not vim.loop.fs_stat(mini_path) then
+	vim.cmd('echo "Installing `mini.nvim`" | redraw')
+	local clone_cmd = {
+		'git', 'clone', '--filter=blob:none',
+		-- Uncomment next line to use 'stable' branch
+		-- '--branch', 'stable',
+		'https://github.com/echasnovski/mini.nvim', mini_path
+	}
+	vim.fn.system(clone_cmd)
+	vim.cmd('packadd mini.nvim | helptags ALL')
+	vim.cmd('echo "Installed `mini.nvim`" | redraw')
+end
+-- Early setup
+vim.g.mapleader = ' '       -- default
+vim.g.maplocalleader = ' '  -- default
+vim.o.relativenumber = true -- and relative line number
+vim.g.virtTextShow = true   -- LSP
+
+-- Set up 'mini.deps' (customize to your liking)
+require('mini.deps').setup({ path = { package = path_package } })
+
+-- Use 'mini.deps'. `now()` and `later()` are helpers for a safe two-stage
+-- startup and are optional.
+local add, now, later = MiniDeps.add, MiniDeps.now, MiniDeps.later
+
+-- Safely execute immediately
+now(function()
+	vim.o.termguicolors = true
+	-- vim.cmd('colorscheme randomhue')
+	add({ source = 'Mofiqul/dracula.nvim' })
+	require('dracula').setup {
+		italic_comment = true,
+		transparent_bg = true,
+		lualine_bg_color = "#282a36",
+		overrides = {
+			---@diagnostic disable-next-line: missing-fields
+			MatchParen = { underline = false, fg = "#ffcc59", bg = "#2b2a2c" }
+		},
+	}
+
+	vim.cmd.colorscheme('dracula')
+end)
+now(function()
+	require('mini.notify').setup()
+	vim.notify = require('mini.notify').make_notify()
+end)
+now(function() require('mini.basics').setup({ mappings = { windows = true } }) end)
+now(function() require('mini.completion').setup() end)
+now(function()
+	local miniclue = require('mini.clue')
+	miniclue.setup({
+		triggers = {
+			-- Leader triggers
+			{ mode = 'n', keys = '<Leader>' },
+			{ mode = 'x', keys = '<Leader>' },
+			-- Built-in completion
+			{ mode = 'i', keys = '<C-x>' },
+			-- `g` key
+			{ mode = 'n', keys = 'g' },
+			{ mode = 'n', keys = '[' },
+			{ mode = 'n', keys = '\\' },
+			{ mode = 'n', keys = ']' },
+			{ mode = 'x', keys = 'g' },
+			-- Marks
+			{ mode = 'n', keys = "'" },
+			{ mode = 'n', keys = '`' },
+			{ mode = 'x', keys = "'" },
+			{ mode = 'x', keys = '`' },
+			-- Registers
+			{ mode = 'n', keys = '"' },
+			{ mode = 'x', keys = '"' },
+			{ mode = 'i', keys = '<C-r>' },
+			{ mode = 'c', keys = '<C-r>' },
+			-- Window commands
+			{ mode = 'n', keys = '<C-w>' },
+			-- `z` key
+			{ mode = 'n', keys = 'z' },
+			{ mode = 'x', keys = 'z' },
+		},
+		clues = {
+			-- Enhance this by adding descriptions for <Leader> mapping groups
+			miniclue.gen_clues.builtin_completion(),
+			miniclue.gen_clues.g(),
+			miniclue.gen_clues.marks(),
+			miniclue.gen_clues.registers(),
+			miniclue.gen_clues.windows(),
+			miniclue.gen_clues.z(),
+		},
+	})
+end)
+now(function() require('mini.icons').setup() end)
+now(function() require('mini.statusline').setup() end)
+
+-- Safely execute later
+later(function() require('mini.ai').setup() end)
+later(function() require('mini.comment').setup() end)
+later(function() require('mini.diff').setup() end)
+later(function() require('mini.extra').setup() end)
+later(function() require('mini.git').setup({}) end)
+later(function() require('mini.pick').setup() end)
+later(function() require('mini.surround').setup() end)
+
+later(function()
+	add({
+		source = 'nvim-treesitter/nvim-treesitter',
+		hooks = { post_checkout = function() vim.cmd('TSUpdate') end },
+	})
+	-- Possible to immediately execute code which depends on the added plugin
+	require('nvim-treesitter.configs').setup({
+		ensure_installed = { 'lua', 'vimdoc' },
+		highlight = { enable = true },
+		incremental_selection = {
+			enable = true,
+			keymaps = {
+				init_selection = '<c-space>',
+				node_incremental = '<c-space>',
+				scope_incremental = '<c-s>',
+				node_decremental = '<M-space>',
+			},
+		},
+		textobjects = {
+			move = {
+				enable = true,
+				set_jumps = true,
+				goto_next_start = {
+					[']m'] = '@function.outer',
+					[']]'] = '@class.outer',
+				},
+				goto_next_end = {
+					[']M'] = '@function.outer',
+					[']['] = '@class.outer',
+				},
+
+			}
+		}
+	})
+	add({
+		source = 'nvim-treesitter/nvim-treesitter-textobjects',
+		depends = { 'nvim-treesitter/nvim-treesitter' }
+	})
+
+	-- Git management
+	add({
+		source = 'lewis6991/gitsigns.nvim',
+		depends = { 'nvim-lua/plenary.nvim' },
+	})
+end)
+
+later(function()
+	add({
+		source = 'stevearc/conform.nvim'
+	})
+	require('conform').setup({ formatters_by_ft = { sh = { "shfmt" } } })
+end)
+
+-- LSP
+vim.lsp.enable({ 'clangd', 'bashls', 'lua_ls' })
+
+-- keymaps
+local C = function(cmd) return "<Cmd>" .. cmd .. "<CR>" end
+local nmp = function(key, cmd, desc) vim.keymap.set('n', key, cmd, { desc = desc }) end
+nmp('<C-N>', C('cnext'), 'qflist next')
+nmp('<C-P>', C("cprev"), 'qflist prev')
+nmp('<ESC>', C("nohlsearch"), 'Clear search highlight')
+nmp('<leader>?', C("Pick keymaps"), '? keymaps')
+nmp('<leader>f', C("Pick files"), 'Pick Files')
+nmp('<leader>gf', C("Pick git_files"), 'Pick Git Files')
+nmp('<leader>gh', C("Pick git_hunks"), 'Pick Git Hunks')
+nmp('<leader>o', C("Pick oldfiles"), 'Pick Oldfiles')
+
+-- autocommands
+local user_group = vim.api.nvim_create_augroup('UserCommands', { clear = true })
+-- lsp buffer attached
+local setqf = function()
+	vim.fn.setqflist(vim.diagnostic.toqflist(vim.diagnostic.get()), 'r')
+	vim.api.nvim_command('botright cwindow');
+end
+local toggle_hints = function()
+	vim.diagnostic.config({
+		virtual_text = not vim.diagnostic.config().virtual_text,
+		virtual_lines = not
+		    vim.diagnostic.config().virtual_lines,
+	});
+end
+vim.api.nvim_create_autocmd('LspAttach', {
+	group = user_group,
+	callback = function(args)
+		local nmpb = function(key, cmd, desc) vim.keymap.set('n', key, cmd, { desc = desc, buffer = args.buf }) end
+		nmpb('grQ', vim.diagnostic.setqflist, 'diagnostic setqflist')
+		nmpb('grq', setqf, 'diagnostic setqflist current buffer')
+		nmpb('grf', function() require 'conform'.format({ lsp_fallback = true }); end, 'Format (buffer)')
+		nmpb('grh', vim.diagnostic.open_float, 'diagnostic float')
+		nmpb('\\H', toggle_hints, 'toggle hints')
+	end,
+})
+-- fix cursor
+vim.api.nvim_create_autocmd('VimLeave', {
+	group = user_group,
+	command = 'set guicursor= | call chansend(v:stderr, "\x1b[ q")'
+})
