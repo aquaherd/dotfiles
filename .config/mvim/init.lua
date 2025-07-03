4c5d24fd8498820eb45e7d28e0e7fb9fa81ff7a6
Hakan Erduman <hakan.erduman@siemens-healthineers.com>
2025-07-03 20:20:41 +0200

    mvim: osc52

--- .config/mvim/init.lua
+++ .config/mvim/init.lua
@@ -228,17 +228,22 @@ end)
 -- Somethings
 later(function()
 	vim.opt.clipboard = 'unnamedplus'
-	if vim.env.SSH_TTY then
-		vim.g.clipboard = {
-			name = 'OSC 52',
-			copy = {
-				['+'] = require('vim.ui.clipboard.osc52').copy '+',
-				['*'] = require('vim.ui.clipboard.osc52').copy '*',
-			},
-			paste = {
-				['+'] = require('vim.ui.clipboard.osc52').paste '+',
-				['*'] = require('vim.ui.clipboard.osc52').paste '*',
-			},
-		}
-	end
+	local function paste() return {
+		vim.split(vim.fn.getreg(''), '\n'),
+		vim.fn.getregtype(''),
+	}
+end
+if vim.env.SSH_TTY then
+	vim.g.clipboard = {
+		name = 'OSC 52',
+		copy = {
+			['+'] = require('vim.ui.clipboard.osc52').copy '+',
+			['*'] = require('vim.ui.clipboard.osc52').copy '*',
+		},
+		paste = {
+			['+'] = paste,
+			['*'] = paste,
+		},
+	}
+end
 end)
