8ccafb0801edd9f90e3cc2e220f2673721f9c85d
Hakan Erduman <hakan.erduman@siemens-healthineers.com>
2025-07-08 18:41:15 +0200

    move mvim from alias to bin

--- .bashrc.d/shared-aliases.sh
+++ .bashrc.d/shared-aliases.sh
@@ -4,7 +4,6 @@ alias cfg='git --git-dir=$HOME/.cfg --work-tree=$HOME '
 alias curr='cat "$XDG_RUNTIME_DIR/host" 2> /dev/null || echo none'
 alias quit='ssh -O exit '
 alias rnvim='nvim --remote-ui --server 127.0.0.1:8192 '
-alias mvim='NVIM_APPNAME=mvim nvim'
 
 colors()
 {
