45803163b568241f78d7ac69e4a836d27da283d3
Hakan Erduman <hakan@erduman.de>
2025-11-27 19:37:02 +0100

    quitall: ssh mux killer alias

--- .bashrc.d/shared-aliases.sh
+++ .bashrc.d/shared-aliases.sh
@@ -4,6 +4,7 @@ alias cfg='git --git-dir=$HOME/.cfg --work-tree=$HOME '
 alias curr='cat "$XDG_RUNTIME_DIR/host" 2> /dev/null || echo none'
 alias quit='ssh -O exit '
 alias drop='ssh-add -d - < ~/.ssh/encr.txt'
+alias quitall='pkill -u $USER -f -HUP '\''\[mux\]'\'''
 alias rnvim='nvim --remote-ui --server 127.0.0.1:8192 '
 
 colors() {
