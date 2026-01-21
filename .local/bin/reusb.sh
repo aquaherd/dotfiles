dc5dfb1ca0b3ad1e2d10c00efae249d44d2ca2a4
Hakan Erduman <hakan@erduman.de>
2026-01-21 11:51:04 +0100

    reusb.sh: load kernel module on demand

--- .local/bin/reusb.sh
+++ .local/bin/reusb.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+if lsmod | grep -vq vhci_hcd; then
+	sudo modprobe vhci-hcd
+fi
 for b in $(sudo usbip list -r localhost 2>/dev/null | grep -oP '\d+-\d+'); do
 	echo "attaching UART $b"
 	sudo usbip attach -r localhost -b "$b"
