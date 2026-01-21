88c0a248cc5537579da8f4802a3c03faea913754
Hakan Erduman <hakan.erduman@siemens-healthineers.com>
2026-01-21 11:49:29 +0100

    no http_proxy for wsl

--- .bashrc.d/zsc.sh
+++ .bashrc.d/zsc.sh
@@ -1,3 +1,9 @@
+if grep -q microsoft /proc/version; then
+	return
+fi
+if [[ -n "$DEV_CONTAINER_NAME" ]] || [[ -n "$container" ]]; then
+        return
+fi 
 if command -v ss > /dev/null; then
 	if ss -lt | grep -q 127.0.0.1:8086; then
 		if ! pidof -q tinyproxy; then
